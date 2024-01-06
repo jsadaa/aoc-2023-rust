6b83ec0bc8c1aada1f9160f45eacc563117a22b8
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2024-01-06 17:40:45 +0100

    reformat day 4

--- src/days/day_5.rs
+++ src/days/day_5.rs
@@ -1,14 +1,11 @@
-use std::collections::BTreeMap;
-use std::ops::Range;
-
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug)]
 pub struct Map {
     name: String,
-    lines: Vec<Vec<u64>>
+    lines: Vec<Vec<u64>>,
 }
 
 impl Map {
-    fn new (name: &str, unprocessed_lines: &[&str]) -> Map {
+    fn new(name: &str, unprocessed_lines: &[&str]) -> Map {
         let processed_lines = Map::proc_lines(name, unprocessed_lines);
         Map { name: name.to_string(), lines: processed_lines }
     }
@@ -33,21 +30,19 @@ impl Map {
 #[derive(Debug)]
 pub struct RangeMap {
     name: String,
-    correlation_map: BTreeMap<u64, u64>,
+    coordinates: Vec<(u64, u64, u64)>,
 }
 
 impl RangeMap {
     fn new(name: String) -> RangeMap {
         RangeMap {
             name,
-            correlation_map: BTreeMap::new(),
+            coordinates: vec![],
         }
     }
 
-    fn add_ranges(&mut self, start: Range<u64>, end: Range<u64>) {
-        for (s, e) in start.zip(end) {
-            self.correlation_map.insert(s, e);
-        }
+    fn add_coordinates(&mut self, start: u64, end: u64, range: u64) {
+        self.coordinates.push((start, end, range))
     }
 }
 
@@ -67,15 +62,15 @@ pub(crate) fn day_5_part_1() {
 
     let mut maps: Vec<Map> = vec![];
     let identifiers: [&str; 7] =
-    [
-        "seed-to-soil",
-        "soil-to-fertilizer",
-        "fertilizer-to-water",
-        "water-to-light",
-        "light-to-temperature",
-        "temperature-to-humidity",
-        "humidity-to-location"
-    ];
+        [
+            "seed-to-soil",
+            "soil-to-fertilizer",
+            "fertilizer-to-water",
+            "water-to-light",
+            "light-to-temperature",
+            "temperature-to-humidity",
+            "humidity-to-location"
+        ];
 
     for identifier in &identifiers {
         let map = Map::new(identifier, &lines);
@@ -86,39 +81,35 @@ pub(crate) fn day_5_part_1() {
 
     for map in &maps {
         let mut range_map = RangeMap::new(map.name.clone());
-        for inst in map.lines.iter() {
-            let range = inst[2];
-            let start = inst[1]..inst[1] + range;
-            let end = inst[0]..inst[0] + range;
-            range_map.add_ranges(start, end);
+        for line in map.lines.iter() {
+            let start = line[1];
+            let end = line[0];
+            let range = line[2];
+            range_map.add_coordinates(start, end, range);
         }
         range_maps.push(range_map);
     }
 
     let mut final_locations: Vec<u64> = vec![];
 
-    for seed in &seeds  {
-        let mut location = seed;
-        println!("seed: {:#?}", seed);
+    for seed in &seeds {
+        let mut location = *seed;
 
-        for identifier in &identifiers  {
-            println!("identifier: {:#?}", identifier);
-            println!("location: {:#?}", location);
+        for identifier in &identifiers {
             if let Some(range_map) = range_maps.iter().find(|&r| r.name == *identifier) {
-                if let Some(v) = range_map.correlation_map.get(location) {
-                    location = v;
+                for (start, end, range) in &range_map.coordinates {
+                    if location >= *start && location <= *start + range {
+                        location = end + (location - start);
+                        break;
+                    }
                 }
             } else {
                 panic!("ERROR: No matching range map with current identifier !")
             }
         }
 
-        final_locations.push(*location);
+        final_locations.push(location);
     }
 
-    println!("seeds: {:#?}", seeds);
-    println!("maps: {:#?}", maps);
-    println!("ranges_maps: {:#?}", range_maps);
-    println!("final_locations: {:#?}", final_locations);
-    println!("lowest final location: {:#?}", final_locations.iter().min());
+    println!("Lowest final location: {:#?}", final_locations.iter().min().unwrap());
 }
